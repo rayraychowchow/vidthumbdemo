515cec7c46194dea1850b4828aa2d4b6dc1c0f66
Ray Chow <raychowiosdev@gmail.com>
2020-09-20 13:57:05 +0800

    Create project and gitignore

---
+++ VidThumbDemo/VidThumbDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VidThumbDemo
+//
+//  Created by Ray Chow on 20/9/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
